8cca4836f39f4aa3d3496b47f955d3cc8fde4824
s33nx <muhseenibrahim5@gmail.com>
2025-11-20 13:42:19 +0100

    The jobs are now coming from json backend. ive added the spinner from react spinners and ive also successfully created the jobs component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom'
 import HomePage from './pages/HomePage';
+import JobsPage from './pages/JobsPage'
 import MainLayout from './layouts/MainLayout';
+import NotFoundPage from './pages/NotFoundPage';
 const router = createBrowserRouter(
   createRoutesFromElements(
   <Route  path='/' element={<MainLayout />}>
   <Route index element={<HomePage />} /> 
+  <Route path='/jobs' element={<JobsPage />} />
+  <Route path='*' element={<NotFoundPage />} /> 
   </Route>
 )
 );
